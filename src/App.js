ec46bb5d45f1faabd48028fbad62b5a061617db9
MPrashant1100 <prashantmishra1100@gmail.com>
2023-05-16 15:51:40 +0530

    Assignment added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import ToDoList from './components/Practice/ToDoList';
+import ShoppingList from './components/Assignment/ShoppingList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ShoppingList/>
+     {/*
       <ToDoList/>
+  */}
     </div>
   );
 }
