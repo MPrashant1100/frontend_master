c8f6a47e5a78f8c092d283c7b11de80b75a8eb17
MPrashant1100 <prashantmishra1100@gmail.com>
2023-05-20 13:53:14 +0530

    new files added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import ToDoList from './components/Practice/ToDoList';
 import ShoppingList from './components/Assignment/ShoppingList';
+import Portfolio from './components/Practice/Portfolio';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ShoppingList/>
+      <Portfolio/>
      {/*
+     <ShoppingList/>
       <ToDoList/>
   */}
     </div>
